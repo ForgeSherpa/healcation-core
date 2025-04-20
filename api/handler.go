d9d51ddf3c067a809f0b9e737aebebedcbe5047d
DJason28 <delvinjason28.tio@gmail.com>
2025-04-20 11:33:45 +0700

    update handler.go

--- api/handler.go
+++ api/handler.go
@@ -3,22 +3,21 @@ package api
 import (
 	"healcationBackend/database"
 	"healcationBackend/routes"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Handler dipanggil Vercel setiap request
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// Muat variabel env & koneksi DB sekali per cold start
 	database.LoadEnvVariables()
 	database.Connect()
 
-	// Buat router Gin
 	gin.SetMode(gin.ReleaseMode)
+	gin.DefaultWriter = io.Discard // Hindari conflict log Vercel
+
 	router := gin.Default()
 	routes.Routes(router)
 
-	// Serve HTTP dengan Gin
 	router.ServeHTTP(w, r)
 }
