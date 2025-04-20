9dddb6afc3ba53716cd38b54fd6a20fd172eb250
DJason28 <delvinjason28.tio@gmail.com>
2025-04-20 11:40:48 +0700

    testing endpoint

--- routes/routes.go
+++ routes/routes.go
@@ -6,11 +6,16 @@ import (
 	"healcationBackend/routes/planner"
 	"healcationBackend/routes/profile"
 	"healcationBackend/routes/travel"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(r *gin.Engine) {
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "API Healcation jalan!"})
+	})
+
 	auth.RoutesAuth(r)
 	history.HistoryRoutes(r)
 	profile.ProfileRoutes(r)
